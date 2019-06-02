50b658dda5da7d34aad3ac04c2448b6cc72bcee8
millerh1 <44813811+millerh1@users.noreply.github.com>
2019-06-02 14:05:09 -0500

    Added set_analysis option to topology analysis

--- tests/Test.R
+++ tests/Test.R
@@ -18,6 +18,11 @@ pairedGenesAnalyzeR(pairedGenesList = pairedGeneList,
                     outputPrefix = "tests/pairedOut")
 
 
+geneList <- c("TORCHIA_TARGETS_OF_EWSR1_FLI1_FUSION_UP")
+result <- analyzeGenesetTopology(genesOfInterest = geneList, Species = "hsapiens",
+                                 crossComparisonType = "variantGenes", outputPrefix = "topology_test1")
+
+
 genesOfInterest <- "F2"
 analyzeSingleGenes(genesOfInterest = genesOfInterest,
                    Sample_Type = "Normal_Tissues", outputPrefix = "tests/newTest")
